f9c380121bb42889e603c303be71876a34c4a8d2
endeavor <endeavor@rainbowsandpwnies.com>
2020-04-24 15:46:21 -0400

    is_function_pure

--- examples/log.py
+++ examples/log.py
@@ -6,14 +6,23 @@ def info(msg):
 def alert(msg):
     print("{}[*] {}{}".format(colored.fg(3), msg, colored.attr(0)))
 
+def warn(msg):
+    alert(msg)
+
 def blue(msg):
-    print("{}[b] {}{}".format(colored.fg(4), msg, colored.attr(0)))
+    print("{}[.] {}{}".format(colored.fg(4), msg, colored.attr(0)))
+
+def cyan(msg):
+    print("{}[.] {}{}".format(colored.fg(6), msg, colored.attr(0)))
 
 def magenta(msg):
-    print("{}[m] {}{}".format(colored.fg(5), msg, colored.attr(0)))
+    print("{}[.] {}{}".format(colored.fg(5), msg, colored.attr(0)))
 
 def error(msg):
     print("{}[-] {}{}".format(colored.fg(9), msg, colored.attr(0)))
 
 def success(msg):
     print("{}[+] {}{}".format(colored.fg(2), msg, colored.attr(0)))
+
+def green(msg):
+    success(msg)
